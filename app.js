e963c0d1413808a18a666f7196b96f9192ee9b15
kittu-20 <sanyam.student.mec19@itbhu.ac.in>
2021-03-15 03:03:53 +0530

    add mongodbAtlas

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost:27017/todolistDB", {useNewUrlParser: true});
+mongoose.connect("mongodb+srv://admin-sanyam:Test123@cluster0.nadih.mongodb.net/todolistDB", {useNewUrlParser: true});
 
 const itemsSchema = {
   name: String
