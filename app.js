3045aa046bf117463bf6e121625df0a1dcdcdd6e
kittu-20 <sanyam.student.mec19@itbhu.ac.in>
2021-03-15 03:11:44 +0530

    h1

--- app.js
+++ app.js
@@ -133,6 +133,14 @@ app.get("/about", function(req, res){
   res.render("about");
 });
 
-app.listen(3000, function() {
-  console.log("Server started on port 3000");
-});
+
+let port = process.env.PORT;
+if (port == null || port == "") {          //heroku ka port
+  port = 3000;                              //apne port pr bhi chalega
+}
+app.listen(port);
+
+
+// app.listen(3000, function() {
+//   console.log("Server started on port 3000");
+// });
